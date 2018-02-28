0d6c2c4fb9c4535d6e1b9fd5d580e93d9130b68a
Rustam Shorov <srmparusa@gmail.com>
2018-02-28 22:37:06 +0100

    O(n)?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if (array.length === 0) return 0;
+
+    function Numsort(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+    }
+
+    let count = 1;
+    let max_count = 1;
+    let sorted_arr = array.sort(Numsort);
+    let set = new Set();
+    set.add(sorted_arr[0]);
+
+    for (let i = 1, length = sorted_arr.length; i < length; i++){
+        if (sorted_arr[i] - sorted_arr[i-1] === 1 || sorted_arr[i] - sorted_arr[i-1] === 0 ) {
+            set.add(sorted_arr[i]);
+            count = set.size;
+            if (count > max_count){ max_count = count; }
+        }
+        else {
+            if (count >= max_count) { max_count = count; }
+            set.clear();
+            set.add(sorted_arr[i]);
+            count = 1;
+        }
+    }
+    return max_count;
 }
